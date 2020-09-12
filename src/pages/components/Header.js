bc8aa4d695c8c0c04d7cfa83bbe7e1026b53d5aa
Leira C Sanchez Quinones <leira.sq@gmail.com>
2020-09-12 17:30:55 -0400

    nothing works

---
+++ src/pages/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
